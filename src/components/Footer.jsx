f63e82350a2bef9c6140e4a8433c78f21d3bbbfd
Daryll Mamaril <daryll.mamaril@avanade.com>
2024-10-03 20:38:19 +1000

    Add Logo and Counter components inside the Header component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <footer>
       <small>&copy; 2024 Copyright. Dazz Mamaril.</small>
-      <p>Version <b>1.5</b></p>
+      <p>
+        Version <b>1.5</b>
+      </p>
     </footer>
   );
 }
