f6bc752861932567c5e030869d3e1399b0831b3b
Daryll Mamaril <daryll.mamaril@avanade.com>
2024-10-11 10:40:42 +1100

    Add initial items to ItemList component

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { initialItems } from "../lib/constants";
+
 export default function ItemList() {
+  const [items, setItems] = useState(initialItems);
+
   return (
-    <div>ItemList</div>
+    <ul>
+      {items.map(item => (
+        <Item key={item.id} item={item}/>
+      ))}
+    </ul>
+  )
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label><input type="checkbox"></input>{item.name}</label>
+      <button>❌</button>
+    </li>
   )
 }
---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const initialItems = [
+  { id: 1, name: "goodmood", packed: true },
+  { id: 2, name: "passport", packed: false },
+  { id: 3, name: "phone charger", packed: false },
+];
+
+export const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
