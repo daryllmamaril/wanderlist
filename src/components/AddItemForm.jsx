2c7a3f782aceee57372f3eb716fb76c57a53c31d
Daryll Mamaril <daryll.mamaril@avanade.com>
2024-10-11 14:52:10 +1100

    Add functionality of adding an item to list

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,11 +1,48 @@
+import { useState, useRef } from "react";
 import Button from "./Button";
 
-export default function AddItemForm() {
+export default function AddItemForm({ setItems }) {
+  const [itemText, setItemText] = useState("");
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!itemText) {
+      alert(
+        "Oops! Item can't be empty. Please enter the item name to add it to your list."
+      );
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+
+    setItems((prev) => [...prev, newItem]);
+    setItemText("");
+    inputRef.current.focus();
+  };
+
   return (
-    <form>
-        <h2>Add an item</h2>
-        <input type="text" placeholder="Toothbrush..." />
-        <Button>Add to list</Button>
+    <form
+      onSubmit={handleSubmit}
+    >
+      <h2>Add an item</h2>
+      <input
+        ref={inputRef}
+        value={itemText}
+        onChange={(e) => {
+          setItemText(e.target.value);
+        }}
+        type="text"
+        placeholder="Toothbrush..."
+        autoFocus
+      />
+      <Button>Add to list</Button>
     </form>
-  )
+  );
 }
