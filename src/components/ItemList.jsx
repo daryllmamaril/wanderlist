f5cc17782827bbbe82d3423e236f319c7fdc5bf1
Daryll Mamaril <daryll.mamaril@avanade.com>
2024-10-15 11:45:21 +1100

    Add react-select and add sorting feature for ItemList

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import EmptyView from "./EmptyView";
+import Select from "react-select";
+
+const sortingOptions = [
+  { label: "Sort by default", value: "default" },
+  { label: "Sort by packed", value: "packed" },
+  { label: "Sort by unpacked", value: "unpacked" },
+]
 
 export default function ItemList({
   handleDeleteItem,
   handleToggleItem,
   items,
 }) {
+
+  const [sortBy, setSortBy] = useState("default");
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return;
+  })
+
   return (
     <ul className="item-list">
       {items.length === 0 && <EmptyView />}
-      {items.map((item) => (
+
+      {items.length > 0 ? (
+        <section className="sorting">
+          <Select onChange={option => setSortBy(option.value)} defaultValue={sortingOptions[0]} options={sortingOptions} />
+        </section>
+      ) : null}
+
+      {sortedItems.map((item) => (
         <Item
           onDeleteItem={handleDeleteItem}
           onToggleItem={handleToggleItem}
